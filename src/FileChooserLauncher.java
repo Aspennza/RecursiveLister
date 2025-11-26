ebacf028c7e09713bf5e77641bc182602a86a45d
Zoe Aspenns <aspennza@mail.uc.edu>
2025-11-26 11:31:02 -0500

    Added a FileChooserLauncher class and modified it so it only allowed the selection of directories

---
+++ src/FileChooserLauncher.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.io.File;
+import java.nio.file.Path;
+
+/**
+ * Allows the creation of objects containing a JFileChooser for selecting
+ * directories, basic methods for prompting the user to select a directory,
+ * and a method for clearing the chooser.
+ * @author Zoe Aspenns aspennza@mail.uc.edu
+ */
+public class FileChooserLauncher
+{
+    //This JFileChooser is used to prompt the user to pick a directory
+    private JFileChooser chooser;
+
+    //This constructor initializes the chooser and sets its default directory
+    public FileChooserLauncher() {
+        chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        chooser.setCurrentDirectory(workingDirectory);
+    }
+
+    /**
+     * This method prompts the user to choose a file, returning the File if they pick one and returning null if they don't
+     * @return a File representing the user's chosen file
+     */
+    public File chooseFile() {
+        if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            return chooser.getSelectedFile();
+        }
+        return null;
+    }
+
+    /**
+     * This method initializes a new JFileChooser and resets its directory
+     */
+    public void resetChooser() {
+        chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+    }
+
+    public JFileChooser getChooser() {
+        return chooser;
+    }
+}
